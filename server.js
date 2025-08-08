d487d8589025136ae9e5c1be526d1d234350032e
NathanEstudanteIF <nptestudante@gmail.com>
2025-08-08 14:50:45 -0300

    feat: implement app middlewares Implement CORS options as segmented file, json native parser and urlencoded.

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-import express, { application } from "express";
+import express from "express";
+import cors from "cors";
+import corsOptions from "./config/corsOptions";
 
 const app = express();
 
-const PORT = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
+
+const PORT = 8081;
 
 app.listen(PORT, () => {
     console.log(`Application running on port ${PORT}`);
